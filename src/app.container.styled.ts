8de5aa658a36908934bf6bf1aa8678751fd51d32
frangaliana <fjgalianacano@gmail.com>
2019-03-12 12:05:38 +0100

    create-navigation-bar: modified structure of app component and added content-container component

---
+++ src/app.container.styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.div`
+  display: grid;
+  grid-template-areas: '"navigation-bar-item" "content-container-item"';
+  grid-template-rows: 3em auto;
+`;
--- src/app.container.tsx
+++ src/app.container.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
+import { StyledContainer } from './app.container.styled';
+import { NavigationBar, ContentContainer } from './components';
 
 interface AppProps {}
 
-export const App: React.FunctionComponent<AppProps> = () => <>Music Player Test</>;
+export const App: React.FunctionComponent<AppProps> = () => (
+  <StyledContainer>
+    <NavigationBar />
+    <ContentContainer />
+  </StyledContainer>
+);
